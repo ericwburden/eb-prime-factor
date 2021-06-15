10eac63ed3af4317c8207f98d35f34342069a8c7
Eric Burden <eric.w.burden@gmail.com>
2021-06-15 06:56:20 -0500

    Update documentation

--- eb_prime_factor/sidebar-items.js
+++ eb_prime_factor/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["is_prime","Determine whether a number is prime"],["main",""],["next_prime","Find next prime in sequence"],["prime_factors","Calculate prime factors"]]});
+initSidebarItems({"fn":[["is_prime","Determine whether a number is prime"],["main","Calculate and display prime factors"],["next_prime","Find next prime in sequence"],["prime_factors","Calculate prime factors"]]});
