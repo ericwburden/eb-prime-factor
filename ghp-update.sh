01e21035a29bb8184bcd61d4e23651ad8f25bbb2
Eric Burden <eric.w.burden@gmail.com>
2021-06-15 06:47:03 -0500

    Corrected package name in ghp-update script

--- ghp-update.sh
+++ ghp-update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-LIB='eb-prime-factor'
+LIB='eb_prime_factor'
 
 cargo doc \
     && echo "<meta http-equiv=refresh content=0;url=$LIB/index.html>" > target/doc/index.html \
-    && ~/.local/bin/ghp-import -n target/doc
+    && ~/.local/bin/ghp-import -n target/doc \
+    && git push origin gh-pages:gh-pages
